012d4710f927ad589f792fb2cfe77b8d42014f55
JoYehyun <jo990714@naver.com>
2023-11-27 23:49:26 +0900

    WEEK1_타겟넘버_BFS

--- GraphTraversal/타겟넘버/타겟넘버_yh.py
+++ GraphTraversal/타겟넘버/타겟넘버_yh.py
@@ -1,3 +1,19 @@
+from collections import deque
+
+def bfs(numbers, target):
+    que = deque()
+    que.append((numbers[0],0))
+    que.append((numbers[0] * (-1), 0))
+
+    while que:
+        num,idx = que.popleft()
+        if idx == len(numbers) - 1:
+            break
+        que.append((num + numbers[idx+1], idx+1))
+        que.append((num - numbers[idx+1], idx+1))
+    
+    return sum(1 for num, idx in que if num == target)
+
 
 def dfs(numbers, target, depth):
     answer = 0
@@ -13,8 +29,10 @@ def dfs(numbers, target, depth):
     return answer
 
 def solution(numbers, target):
-    answer = dfs(numbers,target,0)
+    # answer = dfs(numbers,target,0)
+    answer = bfs(numbers,target)
     return answer
 
 
-print(solution([1, 1, 1, 1, 1], 3))
+print(solution([1, 1, 1, 1, 1], 3)) # 5
+print(solution([4, 1, 2, 1], 4)) # 2
